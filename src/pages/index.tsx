5c7df9829f54a8bde15d570f7a4524ca9955a090
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-07-06 21:44:05 -0300

    add component home page and styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,29 @@
 import Head from "next/head";
+import Image from "next/image";
+import avatar from "../../public/images/avatar.svg";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Hello <span>World</span>
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about <br />
+            the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <Image src={avatar} alt="Girl coding" />
+      </main>
     </>
   );
 }
